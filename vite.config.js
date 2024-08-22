ef3e827817dc9de8c931de34b098d88d009478e5
Serhii01010101 <jobjob2109@gmail.com>
2024-08-22 15:10:49 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(async ({ command }) => {
+  const glob = await import('glob'); // Использование динамического импорта
+
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
@@ -12,8 +12,9 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
+
       rollupOptions: {
-        input: glob.sync('./src/*.html', { absolute: true }).matches,
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -25,12 +26,6 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
